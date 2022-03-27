7b17dce7b974e0e8500f55b57edb87f69a7d5cb4
shapzo <rodrigopalma98@outlook.com>
2022-03-27 16:50:40 -0600

    BSPwm

--- Install.sh
+++ Install.sh
@@ -97,10 +97,10 @@ install_polybar() {
 install_kitty() {
 	echo -e "\n [*] Installing kitty.conf..."
 	if [[ -d "$KDIR" ]]; then
-		cp $DIR/kitty/* "$KDIR"
+		cp -rf $DIR/Kitty/* "$KDIR"
 	else
 		mkdir -p "$KDIR"
-		cp $DIR/kitty/* "$KDIR"
+		cp -rf $DIR/Kitty/* "$KDIR"
 	fi
 }
 
