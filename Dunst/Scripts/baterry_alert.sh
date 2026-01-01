d3571e284e2d3cc09818b91a637ea008799fcf55
rodrigo <rodrigopalma98@outlook.com>
2026-01-01 17:01:35 -0600

    BSPwm

--- Dunst/Scripts/baterry_alert.sh
+++ Dunst/Scripts/baterry_alert.sh
@@ -4,11 +4,10 @@ THRESHOLD=20
 
 while true; do
     BATTERY=$(cat /sys/class/power_supply/BAT1/capacity | head -n 1)
-    # Estado de la batería (Cargando/Descargando)
     STATUS=$(cat /sys/class/power_supply/BAT1/status | head -n 1)
 
     if [ "$BATTERY" -le "$THRESHOLD" ] && [ "$STATUS" != "Charging" ]; then
-        notify-send -u critical "⚠️ Low Battery" "¡Solo queda $BATTERY%! Only Connect the charger." -t 10000
+        notify-send -u critical "Low Battery" "$BATTERY%! Only Connect the charger." -t 10000
         sleep 300
     else
         sleep 60
