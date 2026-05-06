2c59da5806396d4c89017166d0a96df2d385d462
rodrigo <rodrigopalma98@outlook.com>
2026-05-05 19:24:35 -0600

    keybindings

--- Zsh/7-keybindings.zsh
+++ Zsh/7-keybindings.zsh
@@ -4,6 +4,9 @@ bindkey '^[[3;2~' kill-line           # Alt+Del
 bindkey '^[[1;3D' backward-word       # Alt+←
 bindkey '^[[1;3C' forward-word        # Alt+→
 
+# Global aliases expading Ctrl+Espace
+bindkey '^ ' _expand_alias
+
 #================ pluguin autosuggest==============
 bindkey '^[[1;5C' autosuggest-partial-accept  # Ctrl+→
 bindkey '^[[3;5~' autosuggest-clear           # Ctrl+Del
