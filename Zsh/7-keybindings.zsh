08278f433ca20d6dfd0b469daae0c7b9df9299c1
rodrigo <rodrigopalma98@outlook.com>
2026-08-08 20:44:00 -0600

    keybindings

--- Zsh/7-keybindings.zsh
+++ Zsh/7-keybindings.zsh
@@ -14,7 +14,7 @@ bindkey '^[[3;5~' autosuggest-clear           # Ctrl+Del
 #================ pluguin zoxide================
 # Register as a ZLE widget and bind to Ctrl+Z
 zle -N zfz_zoxide
-bindkey '^z' zfz_zoxide
+bindkey '^[z' zfz_zoxide
 # Register the widget and bind it to Ctrl+F (Find)
 zle -N fz_edit_file
 bindkey '^f' fz_edit_file
